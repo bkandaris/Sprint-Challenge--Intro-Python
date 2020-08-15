d531267ea0b99e43481df4fa48222a8ffed90b33
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-08-14 21:30:19 -0400

    MVP

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# This is our base class 'Vehicle' which all other classes will inherit from
+
+class Vehicle:
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
